e0940493a63238811278dcbb82c24ead88a0478c
Jerson Brito <jersonlbrito@hotmail.com>
2023-01-20 00:40:29 -0300

    feat: add icons svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bea-frontend';
+  title = 'Brito & Associados';
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private donSanitizer: DomSanitizer) {
+    var path = (name:string) => `../assets/${name}.svg`;
+    this.matIconRegistry.addSvgIcon('instagran', this.donSanitizer.bypassSecurityTrustResourceUrl(path('instagran')));
+    this.matIconRegistry.addSvgIcon('linkedin', this.donSanitizer.bypassSecurityTrustResourceUrl((path('linkedin'))));
+
+  }
 }
